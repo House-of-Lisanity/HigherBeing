2131f53a7cf352f96fc1fd477d35eb57fa5bdb4f
LisaJ5472 <lisaj5472@gmail.com>
2025-08-14 11:09:57 -0600

    added comment to redeploy

--- server/src/cloudinary.config.ts
+++ server/src/cloudinary.config.ts
@@ -6,3 +6,4 @@ dotenv.config();
 export const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/upload`;
 export const UPLOAD_PRESET = process.env.CLOUDINARY_UPLOAD_PRESET || "";
 export const UPLOAD_FOLDER = process.env.CLOUDINARY_FOLDER || "";
+//for deploy only
